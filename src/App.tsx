f10e90d7d6bc4b934edef88ce569fbc0613387ab
YvvY <momboisse.yvn@gmail.com>
2022-11-08 06:44:03 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Grommet, Page, PageContent, Text, Grid, Box } from "grommet";
+import "./index.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grommet plain full>
+      <Page kind="full" fill height="100%">
+        <PageContent fill pad="none" basis="full">
+          <Grid columns={{ count: 4, size: "small" }} gap="xxsmall" fill>
+            <Box background="brand">
+              <Text>Lorem ipsum stuff</Text>
+            </Box>
+            <Box background="brand">
+              <Text>Lorem ipsum stuff</Text>
+            </Box>
+            <Box background="brand">
+              <Text>Lorem ipsum stuff</Text>
+            </Box>
+            <Box background="brand">
+              <Text>Lorem ipsum stuff</Text>
+            </Box>
+            <Box background="brand">
+              <Text>Lorem ipsum stuff</Text>
+            </Box>
+            <Box background="brand">
+              <Text>Lorem ipsum stuff</Text>
+            </Box>
+            <Box background="brand">
+              <Text>Lorem ipsum stuff</Text>
+            </Box>
+            <Box background="brand">
+              <Text>Lorem ipsum stuff</Text>
+            </Box>
+            <Box background="brand">
+              <Text>Lorem ipsum stuff</Text>
+            </Box>
+            <Box background="brand">
+              <Text>Lorem ipsum stuff</Text>
+            </Box>
+          </Grid>
+        </PageContent>
+      </Page>
+    </Grommet>
   );
 }
 
